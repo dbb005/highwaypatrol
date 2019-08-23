74919323f77fbbf1aa0307ee7f2295570cfec959
Daniel Baker <baker.2442@glenn.it.osu.edu>
2019-08-23 14:40:54 -0400

    This code will let you do all posts

--- code/oshp.R
+++ code/oshp.R
@@ -1,105 +1,77 @@
 library(tidyverse)
-install.packages("ggplot2")
-install.packages("tidycensus")
 library(ggplot2)
 library(tidyverse)
-library(tidycensus)
 library(tigris)
 library(maptools)
 library(sf)
 options(tigris_class = "sf")
 options(tigris_use_cache = TRUE)
-###
-setwd("D:/HP/Maps")
-###
-
-ggplot() +
-  geom_sf(data = oh.st) + 
-  geom_sf(data = oshp.places)
-
-deptaverages <- read_csv("database_deptaverages.csv")
-
-### Andrew Code
-ditch_the_axes <- theme(
-  axis.text = element_blank(),
-  axis.line = element_blank(),
-  axis.ticks = element_blank(),
-  panel.border = element_blank(),
-  axis.title = element_blank()
-)
-oshp <- read_csv("oshp.csv")
-response <- read_csv("response.csv")
-posts <- read_csv("posts.csv") %>%
+# Read In Data ------------------------------------------------------------
+response <- read_csv("data/response.csv")
+oshp <- read_csv("data/OSHP.csv") %>% 
+  rename(postname = NAME)
+avg <- read_csv("data/database_deptaverages.csv")
+posts <- read_csv("data/posts.csv") %>%
   select(-DISTRICT) %>%
   mutate(PRATE = RESP/INDIV)
-###
-oh.places <- places(cb = TRUE, state = "oh")
-oh.st <- states(cb = TRUE, resolution = "20m") %>% filter_state("Ohio")
-###
-oh.ctys <- counties(cb = TRUE, state = "oh") %>%
+# Merges ------------------------------------------------------------------
+postids <- oshp %>% filter(POST>0) %>% select(POST) %>% pull()
+df <- avg %>% filter(POST %in% postids) %>% write_csv("data/df.csv")
+ctys <- counties(cb = TRUE, state = "oh") %>%
   mutate(FIPS = as.numeric(paste0(STATEFP,COUNTYFP)))
+posts <- oshp %>%
+  full_join(.,ctys, by = "FIPS") %>% 
+  select(-(POST2:COUNTYNS),-(AFFGEOID:LSAD)) %>% 
+  left_join(., df, by = "POST") %>% 
+  st_as_sf()
 
-districts <- oshp %>%
-  select(-STATE,-(POST:GHQ)) %>%
-  full_join(.,response, by = "DISTRICT") %>%
-  select(-RESP_RATE) %>%
-  mutate(RATE = RESP/INDIV) %>%
-  full_join(.,oh.ctys, BY = "FIPS") %>%
-  distinct()
-### FOR POSTS
-oshp.places <- inner_join(oh.places,oshp, by = "NAME") %>%
+# Maps --------------------------------------------------------------------
+ggplot() +
+  geom_sf(data = posts,
+          aes(fill = avgage),
+          color = "black") +
+  scale_fill_gradient(high = "#4a1486", #
+                      low = "#dadaeb", 
+                      guide = "colorbar") +
+  #http://colorbrewer2.org/#type=sequential&scheme=Purples&n=7
+  ggtitle("Average Age Across OSHP Posts", 
+          subtitle = "(By District and Post)") +
+  labs(fill = "Average Age") +
+  theme_void() +
+  ggsave("plots/map_avgage.png", width = 10)
+
+# Posts using Lat/Lon -----------------------------------------------------
+cities <- places(cb = TRUE, state = "oh") %>% 
+  rename(postname = NAME) %>% 
+  inner_join(.,oshp, by = ("postname")) %>%
   full_join(.,posts, by = "POST") %>%
   mutate(centroids = st_centroid(geometry)) 
-
-latlon <- oshp.places$centroids
+latlon <- cities$centroids
 latlon <- data.frame(t(sapply(latlon,c)))
-oshp.dots <- cbind(as.data.frame(oshp.places),as.data.frame(latlon))%>%
+dots <- cbind(as.data.frame(cities),as.data.frame(latlon))%>%
   mutate(Y = X2,
          X = X1,
          X2 = NULL,
-         X1 = NULL) %>%
+         X1 = NULL) %>% 
+  left_join(., df, by = "POST") %>%
   st_as_sf()
-#geom_sf(data = oh.st) +
 
+# Maps, round 2 -----------------------------------------------------------
 ggplot() +
-  geom_sf(data = districts,
-          aes(fill = RATE),
-          color = "black") +
-  labs(fill = "District", size = "Post") +
-  scale_fill_gradient(high = "#e6550d", 
-                      low = "#fee6ce", 
-                      guide = "colorbar",
-                      labels = percent) +
-  ggtitle("Survey One Response Rate", 
-          subtitle = "(By District and Post)") +
-  theme_bw() +
-  ditch_the_axes + 
-  geom_point(data = oshp.dots, 
-             aes(x = X, y = Y, size = PRATE),
-             color='white',
-             fill = "black",
+  geom_sf(data = posts) +
+  geom_point(data = dots,
+             aes(x = X, y = Y,
+                 fill = avgage),
+             size = 3.5,
              shape = 21) +
-  scale_size(labels = percent)
-
-### Daniel Code
-ditch_the_axes <- theme(
-  axis.text = element_blank(),
-  axis.line = element_blank(),
-  axis.ticks = element_blank(),
-  panel.border = element_blank(),
-  axis.title = element_blank()
-)
-oshp <- read_csv("oshp.csv")
-deptavg <- read_csv("database_deptaverages.csv")
-posts <- read_csv("posts.csv") %>%
-  select(-DISTRICT) %>%
-  mutate(PRATE = RESP/INDIV)
-###
-oh.places <- places(cb = TRUE, state = "oh")
-oh.st <- states(cb = TRUE, resolution = "20m") %>% filter_state("Ohio")
-###
-oh.ctys <- counties(cb = TRUE, state = "oh") %>%
-  mutate(FIPS = as.numeric(paste0(STATEFP,COUNTYFP)))
+  scale_fill_gradient(high = "#99000d", #
+                      low = "#fee5d9", 
+                      guide = "colorbar") +
+  ggtitle("Average Age Across OSHP Posts", 
+          subtitle = "(By District and Post)") +
+  labs(fill = "Average Age") +
+  theme_void() +
+  ggsave("plots/map_avgage.png", width = 10)
 ###
 oshp.places <- inner_join(oh.places,oshp, by = "NAME") %>%
   full_join(.,posts, by = "POST") %>%
