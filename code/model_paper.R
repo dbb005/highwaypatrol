a3eb41167dfd9bdc09a539d696c3f9a9145f470b
Daniel B. Baker <36234292+dbb005@users.noreply.github.com>
2019-09-10 12:44:06 -0400

    Update model_paper.R

--- code/model_paper.R
+++ code/model_paper.R
@@ -1,6 +1,3 @@
-install.packages("tidyverse")
-install.packages("devtools")
-devtools::install_github("andrewvanleuven/rLeuven")
 library(tidyverse)
 library(dplyr)
 library(broom)
@@ -9,42 +6,52 @@ library(readr)
 ###
 df <- read_csv("data/data.csv")
 df_skinny <- df %>% 
-  select(lastname:selfevaluation12,psm5:psm3) %>% 
-  mutate(psm=(psm1+psm2+psm3+psm4+psm5)/5) %>% 
-  mutate_at(vars(selfevaluation2,selfevaluation4,selfevaluation6,
-                 selfevaluation8,selfevaluation10,selfevaluation12),
-            funs(abs(.-6))) %>% 
-  mutate_at(vars(socialdominance3,socialdominance7),
-            funs(abs(.-6))) %>% 
-  mutate(selfevaluation=(selfevaluation2+selfevaluation4+selfevaluation6+
-    selfevaluation8+selfevaluation10+selfevaluation12+
-    selfevaluation1+selfevaluation3+selfevaluation5+
-    selfevaluation7+selfevaluation9+selfevaluation11)/12) %>% 
-  mutate(dom=(socialdominance1+socialdominance2+socialdominance3)/3) %>% 
-  mutate(antiegal=(socialdominance5+socialdominance6+socialdominance7)/3) %>% 
-  select(-c(18:45)) %>% 
   mutate(male=if_else(gender>1, 1, 0, missing = NULL)) %>% 
   mutate(white=if_else(race<1, 1, 0, missing = NULL)) %>% 
   mutate(tenure=(2019-joinyear)) %>% 
-  select(-(gender:race),-(joinyear))
+  select(-(gender:race),-(joinyear)) %>% 
+  select(-(email),-(lastname),-(firstname),-(supname))
+#  select(lastname:selfevaluation12,psm5:psm3) %>% 
+#  mutate(psm=(psm1+psm2+psm3+psm4+psm5)/5) %>% 
+#  mutate_at(vars(selfevaluation2,selfevaluation4,selfevaluation6,
+#                 selfevaluation8,selfevaluation10,selfevaluation12),
+#            funs(abs(.-6))) %>% 
+#  mutate_at(vars(socialdominance3,socialdominance7),
+#            funs(abs(.-6))) %>% 
+#  mutate(selfevaluation=(selfevaluation2+selfevaluation4+selfevaluation6+
+#    selfevaluation8+selfevaluation10+selfevaluation12+
+#    selfevaluation1+selfevaluation3+selfevaluation5+
+#    selfevaluation7+selfevaluation9+selfevaluation11)/12) %>% 
+#  mutate(dom=(socialdominance1+socialdominance2+socialdominance3)/3) %>% 
+#  mutate(antiegal=(socialdominance5+socialdominance6+socialdominance7)/3) %>% 
+#  select(-c(18:45)) %>% 
+
 
 # Tidy Up Data ------------------------------------------------------------
-lts <- df_skinny %>% filter(rank=="Lieutenant") %>% 
-  setNames(paste0('lt_', names(.))) %>% 
-  mutate(stlt_uniqueid=lt_supid)
 sgt <- df_skinny %>% filter(rank=="Sergeant") %>% 
   setNames(paste0('sgt_', names(.))) %>% 
-  mutate(lt_uniqueid=sgt_supid)
+  rename(sup_uniqueid=sgt_supid) %>% 
+  select(1:7, 183:185, everything())
+lts <- df_skinny %>% filter(rank=="Lieutenant") %>% 
+  setNames(paste0('lt_', names(.))) %>% 
+  rename(stlt_uniqueid=lt_supid) %>% 
+  select(-c(204:317)) %>% 
+  select(1:10, 183:185, 11:103)
 stlt <- df_skinny %>% filter(rank=="Staff Lieutenant") %>% 
-  setNames(paste0('stlt_', names(.)))
+  setNames(paste0('stlt_', names(.))) %>% 
+  select(-c(204:317)) %>% 
+  select(-c(2:6)) %>% 
+  select(1:2, 178:180, 3:98)
+ 
+  
 
+lts_join <- left_join(lts, stlt, by = ("stlt_uniqueid")) %>% 
+  rename(sup_uniqueid=lt_uniqueid)
+#sgt_join <- left_join(sgt, lts_join, by = ("lt_uniqueid")) %>%   
+#  select(-(sgt_supname:sgt_supid),-(lt_supname:lt_supid),-(stlt_supname:stlt_supid))
 
-lts_join <- left_join(lts, stlt, by = ("stlt_uniqueid"))
-sgt_join <- left_join(sgt, lts_join, by = ("lt_uniqueid")) %>%   
-  select(-(sgt_supname:sgt_supid),-(lt_supname:lt_supid),-(stlt_supname:stlt_supid))
-write_csv(sgt_join, "data/supervisors_join.csv")
-
-#all_join <- left_join(df_skinny, sgt_join, by = "")
+write_csv(sgt, "data/sgt.csv")
+write_csv(lts_join, "data/supervisors.csv")
 
 
 # example -----------------------------------------------------------------
